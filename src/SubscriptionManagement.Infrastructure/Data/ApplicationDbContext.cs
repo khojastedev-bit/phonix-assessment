7fd592f1208373a9d7cfbe56ac72463a597024a5
reg <regs@gmail.com>
2025-10-20 08:56:14 +0200

    feat: Add Application EF Database Context

---
+++ src/SubscriptionManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubscriptionManagement.Core.Entities;
+
+namespace SubscriptionManagement.Infrastructure.Data;
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<SubscriptionPlan> SubscriptionPlans => Set<SubscriptionPlan>();
+    public DbSet<UserSubscription> UserSubscriptions => Set<UserSubscription>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SubscriptionPlan>(entity =>
+        {
+            entity.HasKey(sp => sp.Id);
+            entity.Property(sp => sp.Name).IsRequired().HasMaxLength(100);
+            entity.Property(sp => sp.Description).HasMaxLength(500);
+            entity.Property(sp => sp.Price).HasPrecision(18, 2);
+            entity.Property(sp => sp.Features).HasConversion(
+                v => string.Join(';', v),
+                v => v.Split(';', StringSplitOptions.RemoveEmptyEntries).ToList()
+            );
+        });
+
+        modelBuilder.Entity<UserSubscription>(entity =>
+        {
+            entity.HasKey(us => us.Id);
+            entity.HasOne(us => us.SubscriptionPlan)
+                .WithMany()
+                .HasForeignKey(us => us.SubscriptionPlanId);
+
+            entity.HasIndex(us => new { us.UserId, us.Status });
+        });
+    }
+}
