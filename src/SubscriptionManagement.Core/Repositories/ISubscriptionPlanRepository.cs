410395607561f6cfb07d6317561c1bb20c0338dd
reg <regs@gmail.com>
2025-10-20 08:05:12 +0200

    feat: Add related repo contracts

---
+++ src/SubscriptionManagement.Core/Repositories/ISubscriptionPlanRepository.cs
@@ -0,0 +1,10 @@
+﻿using SubscriptionManagement.Core.Entities;
+
+namespace SubscriptionManagement.Core.Repositories;
+public interface ISubscriptionPlanRepository
+{
+    Task<SubscriptionPlan?> GetByIdAsync(Guid id);
+    Task<IEnumerable<SubscriptionPlan>> GetAllActiveAsync();
+    Task AddAsync(SubscriptionPlan plan);
+    void Update(SubscriptionPlan plan);
+}
---
+++ src/SubscriptionManagement.Core/Repositories/IUserSubscriptionRepository.cs
@@ -0,0 +1,11 @@
+﻿using SubscriptionManagement.Core.Entities;
+
+namespace SubscriptionManagement.Core.Repositories;
+public interface IUserSubscriptionRepository
+{
+    Task<UserSubscription?> GetActiveSubscriptionAsync(Guid userId);
+    Task<UserSubscription?> GetByIdAsync(Guid id);
+    Task<IEnumerable<UserSubscription>> GetUserSubscriptionsAsync(Guid userId);
+    Task AddAsync(UserSubscription subscription);
+    void Update(UserSubscription subscription);
+}
