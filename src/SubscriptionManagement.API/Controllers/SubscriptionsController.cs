158da6d8a31a076f00249f169ce827c5ed792e01
reg <regs@gmail.com>
2025-10-20 09:40:18 +0200

    feat: add controller actions for GetActivePlans and ActivateSubscription and adding some extensibilities point

---
+++ src/SubscriptionManagement.API/Controllers/SubscriptionsController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SubscriptionManagement.Application.Features.SubscriptionPlans.Commands;
+using SubscriptionManagement.Application.Features.SubscriptionPlans.Queries;
+
+namespace SubscriptionManagement.API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SubscriptionsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("plans")]
+    public async Task<IActionResult> GetActivePlans()
+    {
+        var result = await _mediator.Send(new GetAllActivePlansQuery());
+        return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+    }
+
+    [HttpPost("activate")]
+    public async Task<IActionResult> ActivateSubscription(ActivateSubscriptionRequest request)
+    {
+        var command = new ActivateSubscriptionCommand(request.UserId, request.PlanId);
+        var result = await _mediator.Send(command);
+
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : BadRequest(result.Error);
+    }
+
+    [HttpPost("{subscriptionId:guid}/cancel")]
+    public async Task<IActionResult> CancelSubscription(Guid subscriptionId)
+    {
+        //todo: 
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("users/{userId:guid}/active")]
+    public async Task<IActionResult> GetActiveSubscription(Guid userId)
+    {
+        //todo: 
+        throw new NotImplementedException();
+    }
+}
+
+public record ActivateSubscriptionRequest(Guid UserId, Guid PlanId);
